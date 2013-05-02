32ddd746d5d1125e6082631aee494b54d5c55916
Nicholas Henry <nicholas.henry@gmail.com>
2013-05-01 19:36:31 -0700

    Config store

--- store_config.rb
+++ store_config.rb
@@ -4,10 +4,10 @@ class StoreConfig
   end
 
   def self.production_url
-    "http://sample-store-demo.herokuapp.com"
+    "http://shrouded-fortress-5742.herokuapp.com/"
   end
 
   def self.email_address
-    "katrina@jumpstartlab.com"
+    "nicholas.henry@gmail.com"
   end
 end
