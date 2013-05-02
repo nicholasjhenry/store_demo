685aaa2f5dc11e7c27308c1bbe6195c716e1fe91
Nicholas Henry <nicholas.henry@gmail.com>
2013-05-01 22:13:57 -0700

    Update port to work with Puma by default

--- store_config.rb
+++ store_config.rb
@@ -1,13 +1,13 @@
 class StoreConfig
   def self.development_url
-    "http://localhost:3000"
+    "http://localhost:5000"
   end
 
   def self.production_url
     "http://shrouded-fortress-5742.herokuapp.com/"
   end
 
-  def self.email_address
+  def self.email_addres
     "nicholas.henry@gmail.com"
   end
 end
